ee021b51af1c894c78edfd724e86d5c3a068822c
patelhardik5520 <77543451+patelhardik5520@users.noreply.github.com>
2021-03-28 00:51:16 -0400

    Request Formation

--- NutrientDiary/Pages/Index.cshtml.cs
+++ NutrientDiary/Pages/Index.cshtml.cs
@@ -19,10 +19,36 @@ namespace NutrientDiary.Pages
 
         public string Base64Image { get; set; }
 
-        public void OnPost(string base64)
+        public void OnPost(string base64image)
         {
-            Base64Image = base64;
-            String requestJson = "{​\"requests\":[{​\"features\":[{​\"maxResults\":5,\"type\":\"OBJECT_LOCALIZATION\"}​],\"image\":{​\"content\":\"" + Base64Image + "\"}​}​]}​";
+            Base64Image = base64image;
+            var imageParts = base64image.Split(',').ToList<string>();
+
+            VisionAPIRequest visionAPIRequest = new VisionAPIRequest()
+            {
+                requests = new List<requests>()
+                {
+                    new requests()
+                    {
+                        image = new image()
+                        {
+                            content = imageParts[1]
+                        },
+                        features = new List<features>()
+                        {
+                            new features()
+                            {
+                                type= "OBJECT_LOCALIZATION",
+                                maxResults = 5
+                            }
+                        }
+                    }
+                }
+            };
+
+
+
+            String requestJson = JsonConvert.SerializeObject(visionAPIRequest);
 
         }
     }
