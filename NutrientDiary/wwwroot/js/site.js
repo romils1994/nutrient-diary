aaa7ecbd0e7bfcef04d9dc1f6583612cbadcc9eb
Romil Shah <romils1994@gmail.com>
2021-04-14 16:41:54 -0400

    Revert "Food Data Central and Data Feed API Integration"
    This reverts commit 8ea80c830d04a75247a9584f6086e7c722808a80.

--- NutrientDiary/wwwroot/js/site.js
+++ NutrientDiary/wwwroot/js/site.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿Webcam.set({
     width: 320,
     height: 240,
@@ -7,38 +6,8 @@
 });
 Webcam.attach('#my_camera');
 
-=======
-﻿if (typeof Webcam !== 'undefined') {
-    Webcam.set({
-        width: 320,
-        height: 240,
-        image_format: 'jpeg',
-        jpeg_quality: 100
-    });
-    Webcam.attach('#my_camera');
-}
-var foodDetails = {};
->>>>>>> Stashed changes
 $(document).ready(function () {
     $("#submitImage,#Base64Image").hide();
-
-    $(".nutrient-tabs a").click(function () {
-        $(this).tab('show');
-    });
-
-    $(function updateNutrientInfo() {
-        if ($("input[data-food-item]")) {
-            $("input[data-food-item]").each(function (key, val) {
-                var foodItem = $(this).data("food-item");
-                var foodNutrient = new Array();
-                $("table[data-fdc=" + foodItem + "] tbody").find('tr').each(function (key, val) {
-                    var td = $(this).find('td');
-                    foodNutrient[key] = td.eq(1).text();
-                });
-                foodDetails[foodItem] = foodNutrient;
-            });
-        }
-    });
 });
 
 function takeSnapshot() {
@@ -74,18 +43,3 @@ function previewFile() {
     }
     $("#submitImage").show();
 }
-
-$("input[name=portion]").change(function updateNutrientInfo() {
-    var foodItem = $(this).data("food-item");
-    var portionSize = $(this).val();
-
-    $("table[data-fdc=" + foodItem + "] tbody").find('tr').each(function (key, val) {
-        var td = $(this).find('td');
-        var initialAmount = foodDetails[foodItem][key];
-        var updAmount = ((portionSize / 100) * initialAmount).toFixed(2);
-        if (updAmount % 1 === 0) {
-            updAmount = updAmount.toFixed(0);
-        }
-        td.eq(1).text(updAmount);
-    });
-});
