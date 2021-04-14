d6c73d5998ef8c8fbe055be5a613d9b27095151e
Romil Shah <romils1994@gmail.com>
2021-04-14 16:48:19 -0400

    DataFeed Files Added

---
+++ NutrientDiary/Pages/Recipes.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NutrientDiary.Pages
+{
+    public class RecipesModel : PageModel
+    {
+        public List<Recipes.MostViewedRecipe> MostViewedRecipes;
+
+        public string Error;
+        public void OnGet()
+        {
+            String url = "https://localhost:44329/?getMostSearched=true";
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    string mostViewedRecipeString = webClient.DownloadString(url);
+                    MostViewedRecipes = Recipes.MostViewedRecipe.FromJson(mostViewedRecipeString);
+                } catch (Exception ex)
+                {
+                    Error = "Something went wrong! Could not reach the API Servers";
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
